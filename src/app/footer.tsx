ad5668215245aa13cf89014bac536b5555911320
jdaun0621 <455a4555786@gmail.com>
2025-06-13 16:52:18 +0900

    feat: mdx 샘플 추가

--- src/app/footer.tsx
+++ src/app/footer.tsx
@@ -3,7 +3,7 @@ export default function Footer() {
     <footer className='bg-gray-100 py-4'>
       <div className='max-w-6xl mx-auto px-4'>
         <p className='text-center text-sm'>
-          &copy; {new Date().getFullYear()} MDX Blog. All rights reserved.
+          &copy; {new Date().getFullYear()} DAUN Blog. 
         </p>
       </div>
     </footer>
