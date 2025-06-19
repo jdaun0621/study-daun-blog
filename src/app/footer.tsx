06c7feabc438ffa95e1bb2f94103484b04f91a24
jdaun0621 <455a4555786@gmail.com>
2025-06-19 19:55:05 +0900

    feat: mdx 이미지 변경

--- src/app/footer.tsx
+++ src/app/footer.tsx
@@ -2,7 +2,7 @@ export default function Footer() {
   return (
     <footer className='bg-neutral-50 py-4'>
       <div className='max-w-6xl mx-auto px-4'>
-        <p className='text-center text-indigo-500'>
+        <p className='text-center text-gray-500'>
           &copy; {new Date().getFullYear()} DAUN Blog. 
         </p>
       </div>
