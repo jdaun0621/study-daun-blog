04e289e00b67e32b96c4c0489db9f2aaecd9b022
jdaun0621 <455a4555786@gmail.com>
2025-06-24 15:08:22 +0900

    feat: mdx 스타일 변경

--- src/app/footer.tsx
+++ src/app/footer.tsx
@@ -1,14 +1,14 @@
 export default function Footer() {
   return (
-    <footer className='bg-white py-4'>
+    <footer className='bg-indigo-200 py-4'>
       <div className='max-w-6xl mx-auto px-4'>
-        <p className='text-center font-bold text-violet-500 scale-150 h-8'>
+        <p className='text-center font-bold text-gray-600 scale-150 h-8'>
           &copy; {new Date().getFullYear()} DAUN Blog.
         </p>
-        <p className='text-center font-extrabold text-indigo-400 h-8 src = '>
+        <p className='text-center font-extrabold text-gray-500 h-8 src = '>
           Email : 455a4555786@gmail.com
         </p>
-        <p className='text-center font-extrabold text-indigo-400'>
+        <p className='text-center font-extrabold text-gray-500'>
           Number : 010-7635-0621
         </p>
       </div>
